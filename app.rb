9264f39758e6f76d22912ad9e56495cc2801358c
jfagan132 <jfagan@ggc.edu>
2015-09-25 14:25:37 +0000

    altered app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require './lib/semester.rb'
 
 set :public_folder, "public"
 
@@ -7,7 +8,7 @@ get '/' do
 end
 
 post '/semesters/' do
-  erb :semesters, :locals => { :current => params['current'], :num => params['num'] }
+  erb :semesters, :locals => { :current => params['current'], :num => params['num'], :result => Semester.new.next_semesters(params['current'], params['num'])}
 end
 
 get '/about/' do
